11e7ac4552e3ae450bcd1aae657cbec1bc5e91b2
Igor Gayday <gaidai92@gmail.com>
2021-03-15 13:20:43 -0700

    Adds a script to copy output files into results folder

---
+++ ozone/copy_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import shutil
+from pathlib import Path
+
+
+def main():
+    states_path = Path("states.fwc").absolute()
+    target_path_parts = list(states_path.parts)
+    del target_path_parts[-2]
+    k_folder_index = [part.startswith("K_") for part in target_path_parts].index(True)
+    target_path_parts.insert(k_folder_index, "results")
+    target_path = Path(*target_path_parts)
+    target_path.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(states_path, target_path)
+
+
+if __name__ == "__main__":
+    main()
