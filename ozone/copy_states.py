6e193aa2b4b4d7f4cebe6d0672c5fecdc180d874
Igor Gayday <gaidai92@gmail.com>
2021-06-20 15:49:39 -0700

    Updates for new path format

--- ozone/copy_states.py
+++ ozone/copy_states.py
@@ -4,12 +4,13 @@ from pathlib import Path
 
 
 def main():
-    states_path = Path("states.fwc").absolute()
+    """Copies file state_properties.fwc from the working directory to the target directory.
+    Target directory path is formed by inserting 'results' before the J folder and remove stage folder name."""
+    states_path = Path("state_properties.fwc").absolute()
     target_path_parts = list(states_path.parts)
-    del target_path_parts[-2]
-    k_folder_index = [part.startswith("K_") for part in target_path_parts].index(True)
-    target_path_parts.insert(k_folder_index, "results")
-    target_path_parts[-1] = "states.ssdtp"
+    del target_path_parts[-2] # delete stage folder
+    j_part_index = [part.startswith("J_") for part in target_path_parts].index(True)
+    target_path_parts.insert(j_part_index, "results")
     target_path = Path(*target_path_parts)
     target_path.parent.mkdir(parents=True, exist_ok=True)
     shutil.copyfile(states_path, target_path)
