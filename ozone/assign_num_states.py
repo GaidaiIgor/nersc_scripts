0799d9eaa48a5298a20f58cbb7fb7e90b5c1f480
Igor Gayday <gaidai92@gmail.com>
2022-03-25 18:31:28 -0700

    Interpolates states for unknown JK

--- ozone/assign_num_states.py
+++ ozone/assign_num_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import numpy as np
 import math
+import numpy as np
+from scipy.interpolate import griddata
 
 from common import *
 
@@ -9,11 +10,22 @@ sys.path.append('/global/u2/g/gaidai/SpectrumSDT_ifort/scripts/')
 from SpectrumSDTConfig import SpectrumSDTConfig
 
 
+def arrange_interp_data(num_states_ref, Js, Ks):
+    """ Arranges irregular data for interpolation into n x 3 list of points. """
+    J_mesh, K_mesh = np.meshgrid(Js, Ks)
+    J_1d = J_mesh.flatten()
+    K_1d = K_mesh.flatten()
+    num_states_1d = num_states_ref.flatten()
+    interp_data = np.stack((J_1d, K_1d, num_states_1d), axis=1)
+    interp_data = interp_data[interp_data[:, 2] != 0, :]
+    return interp_data
+
+
 def estimate_states(num_states_ref, Js, Ks, J, K, mult):
     """ Estimates necessary number of states for given J and K. Js and Ks are values of J and K for num_states_ref. """
-    J_ind = Js.index(J)
-    K_ind = Ks.index(K)
-    states = int(math.ceil(num_states_ref[K_ind, J_ind] * mult))
+    interp_data = arrange_interp_data(num_states_ref, Js, Ks)
+    states = griddata(interp_data[:, 0:2], interp_data[:, 2], (J, K))
+    states = int(math.ceil(states * mult))
     return states
 
 
@@ -36,8 +48,9 @@ def main():
     molecule = get_ozone_molecule(mass)
     use_half_integers = config.get_half_integers()
     sym_suffix = 'H' if use_half_integers else ''
+    mult = 1 if is_monoisotopomer(molecule) else 1/3
+    mult *= 1.05
     load_path = f'/global/u2/g/gaidai/nersc_scripts/ozone/script_data/num_states/{molecule}/sym_1{sym_suffix}/num_states.txt'
-    mult = 1/3
     num_states_ref = np.loadtxt(load_path)
 
     J = config.get_J()
